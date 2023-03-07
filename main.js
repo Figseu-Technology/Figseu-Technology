10d3d8fb7ee9ea5f264c4da95ac07ef1a90c2042
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2023-03-08 05:16:32 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+let Stages = 0;
+
+function Submit() {
+   Stages = $('#stage-select').val();
+   if (Stages == "61") {
+      location.href = "https://figseu-technology.github.io/Delta/Delta1_u36WTVFd8jpG64.html";
+   } else if (Stages == "62") {
+      location.href = "https://figseu-technology.github.io/Delta/Delta3_j5iR3Ny5ipd9cfD5FQQVH.html";
+   } else if (Stages == "69") {
+      location.href = "https://figseu-technology.github.io/Delta/Delta60_6Yaj6fWSr5wJJznWEjHy224.html";
+   } else {
+      location.href = "https://figseu-technology.github.io/Delta/";
+   }
+}
