86869036657298e16f5c8c2298af95bd229945b2
UD Alice <66378306+Figseu-Technology@users.noreply.github.com>
2023-03-08 18:02:46 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,33 @@ let Stages = 0;
 
 function Submit() {
    Stages = $('#stage-select').val();
-   if (Stages == "61") {
+   if (Stages == "0") {
+      location.href = "https://figseu-technology.github.io/zero_index.html?f=0";
+   } else if (Stages == "1") {
+      location.href = "https://figseu-technology.github.io/one_index.html?f=1";
+   } else if (Stages == "2") {
+      location.href = "https://figseu-technology.github.io/one_index.html?f=2";
+   } else if (Stages == "4") {
+      location.href = "https://figseu-technology.github.io/two_index.html?f=4";
+   } else if (Stages == "5") {
+      location.href = "https://figseu-technology.github.io/two_index.html?f=5";
+   } else if (Stages == "6") {
+      location.href = "https://figseu-technology.github.io/two_index.html?f=6";
+   } else if (Stages == "7") {
+      location.href = "https://figseu-technology.github.io/two_index.html?f=7";
+   } else if (Stages == "8") {
+      location.href = "https://figseu-technology.github.io/two_index.html?f=8";
+   } else if (Stages == "9") {
+      location.href = "https://figseu-technology.github.io/three_index.html?f=9";
+   } else if (Stages == "10") {
+      location.href = "https://figseu-technology.github.io/three_index.html?f=10";
+   } else if (Stages == "11") {
+      location.href = "https://figseu-technology.github.io/three_index.html?f=11";
+   } else if (Stages == "12") {
+      location.href = "https://figseu-technology.github.io/three_index.html?f=12";
+   } else if (Stages == "14") {
+      location.href = "https://figseu-technology.github.io/four_index.html?f=14";
+   } else if (Stages == "61") {
       location.href = "https://figseu-technology.github.io/Delta/Delta1_u36WTVFd8jpG64.html";
    } else if (Stages == "62") {
       location.href = "https://figseu-technology.github.io/Delta/Delta3_j5iR3Ny5ipd9cfD5FQQVH.html";
